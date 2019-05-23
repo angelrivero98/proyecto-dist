7ed17754d01a35185a371de7e09ea9dce433ff2c
Angel Rivero <angelantonio3000@gmail.com>
2019-05-22 23:42:11 -0400

    Inital Commit

---
+++ src/com/company/SendMessageThread.java
@@ -0,0 +1,48 @@
+package com.company;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class SendMessageThread implements Runnable {
+
+    private String value = null;
+    private String message = null;
+    private Integer seconds = 10;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+    public void setSeconds(Integer seconds) {
+        this.seconds = seconds;
+    }
+
+
+    @Override
+    public void run() {
+
+        try {
+            if (this.seconds > 0) {
+                TimeUnit.SECONDS.sleep(10);
+            }
+            ClientMessage sendmessage = new ClientMessage();
+            sendmessage.startConnection(value.substring(5), new Integer(value.substring(0, 4)));
+            sendmessage.sendMessage(this.message);
+
+
+        } catch (InterruptedException ex) {
+            System.out.println("Error (1001): sending -- " + value.substring(5) + " , " + value.substring(0, 4) + " , " + this.message);
+            //Logger.getLogger(SendMessageThread.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            System.out.println("Error (1002): sending -- " + value.substring(5) + " , " + value.substring(0, 4) + " , " + this.message);
+            //Logger.getLogger(SendMessageThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
